75953d3f917a02283322e5bb7fb27be82dc5cd35
PSKutergin <PSKutergin@gmail.com>
2024-06-12 23:39:38 +0300

    added product component and created EventEmitter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { AdvantagesComponent } from './components/advantages/advantages.component';
 import { AdvantageItemComponent } from './components/advantage-item/advantage-item.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AdvantagesComponent,
     AdvantageItemComponent,
+    ProductComponent,
   ],
   imports: [
     BrowserModule,
